4c4141b560fbf82e8a8e56efe154d5ebf533717d
NekrasovaOlga <nekrasova.kalina@yandex.ru>
2022-10-21 12:26:32 +0300

    Create webpack

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+
+const mode = process.env.NODE_ENV || 'development';
+const target = node === 'development' ? 'web' : 'browserlist';
+const devtool = mode === 'development' ? 'source-map' : undefined;
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        hot: true,
+    },
+    entryt: ['@babel/polyfill', './src/index.js'],
+    output: {
+        filename: '[name][contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: 'true',
+        assetModuleFilename: 'assets/[hash][ext][query]',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: '.src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name][contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff2|woff|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.m?js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                    }
+                }
+
+            },
+        ],
+    },
+}
